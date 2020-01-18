601173c413739a194a5169b90f35f9d755be7f7d
Pillár <Miroslav.Pillar@senacor.com>
2020-01-18 18:11:58 +0100

    code splitting of 404 page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import "./App.css";
 import CountriesContainer from "./routes/CountriesContainer";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import NotFoundPage from "./routes/NotFoundPage";
 
 // pages
 const Home = lazy(() => import("./routes/Home"));
 const Favourites = lazy(() => import("./routes/Favourites"));
+const NotFoundPage = lazy(() => import("./routes/NotFoundPage"));
 
 const App = () => {
   return (
